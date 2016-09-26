a4c0e2815c4cfbd50992f8d835ab202c58d10c22
Vytautas Povilaitis <vytautas@veleza.com>
2016-09-26 15:57:38 +0300

    Initial commit

---
+++ src/Veleza/ReaDB/ReaDB.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Veleza\ReaDB;
+
+class ReaDB
+{
+
+    public function __construct($host, $req_port = 0, $pub_port = 0) {
+
+    }
+
+    public function request() {
+        echo 'test';
+    }
+
+}
+
+
