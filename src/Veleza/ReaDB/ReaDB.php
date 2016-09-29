0498bdf6f7721dad06fcaba53018fb7e25d6809e
Vytautas Povilaitis <vytautas@veleza.com>
2016-09-29 14:56:21 +0300

    Improvements

--- src/Veleza/ReaDB/ReaDB.php
+++ src/Veleza/ReaDB/ReaDB.php
@@ -20,53 +20,46 @@ class ReaDB
         $this->host = $host;
         $this->req_port = $req_port;
         $this->sub_port = $sub_port;
+
+        if (!$this->host) {
+            throw new \Exception('ReaDB requires a host parameter to be set!');
+        }
+
+        if ($this->req_port) {
+            $this->context = new \ZMQContext(1);
+            $this->req_socket = new \ZMQSocket($this->context, \ZMQ::SOCKET_REQ);
+            $this->req_socket->connect(sprintf('%s:%s', $this->host, $this->req_port));
+            $this->req_socket->setSockOpt(\ZMQ::SOCKOPT_RCVTIMEO, 5000);
+            $this->req_socket->setSockOpt(\ZMQ::SOCKOPT_LINGER, 0);
+        }
     }
 
+public $reply = null;
+public $duration = 0;
+
     public function request($cmd, $arguments=[]) {
-        $this->connectToReq();
+        $start = microtime(true);
         $msg = [ $cmd, $arguments ];
         $data = snappy_compress(msgpack_pack($msg));
-        $queue->send($data);
+        $reply = $this->req_socket->send($data)->recv();
+        if (!$reply) {
+            throw new \Exception('Request timed out. ');
+        }
+        $result = msgpack_unpack(snappy_uncompress($reply));
+        $this->duration = microtime(true) - $start;
+
+        $this->reply = $reply;
+        return $result;
     }
 
     private function uuid4() {
         return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
             mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
-
-            // 16 bits for "time_mid"
             mt_rand( 0, 0xffff ),
-
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
             mt_rand( 0, 0x0fff ) | 0x4000,
-
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
             mt_rand( 0, 0x3fff ) | 0x8000,
-
-            // 48 bits for "node"
             mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
         );
-    }
-
-    private function createContext() {
-        if (!$this->context) {
-            $this->context = new \ZMQContext(1);
-        }
-    }
-
-    private function connectToReq() {
-        if ($this->req_socket) {
-            return;
-        }
-        if (!$this->host || !$this->req_port) {
-            throw new \Exception('Request requires a host and a req_port options to be set!');
-        }
-        $this->createContext();
-        $this->req_socket = new \ZMQSocket($this->context, \ZMQ::SOCKET_REQ, $this->id);
-        $this->req_socket->connect(sprintf('%s:%s', $this->host, $this->req_port));
     }
 
 }
